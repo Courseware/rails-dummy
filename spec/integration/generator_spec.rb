7af865831ba715efc726856ab7802d31ce637ac1
Stas SUȘCOV <stas@net.utcluj.ro>
2014-02-09 00:57:01 +0200

    Cleanup the integration test.

--- spec/integration/generator_spec.rb
+++ spec/integration/generator_spec.rb
@@ -1,23 +1,24 @@
 require 'spec_helper'
 
 describe Rails::Dummy::Generator do
-  let(:dummy_path) { './dummy_app_path' }
-  let(:full_dummy_path) do
-    spec_folder_path = File.expand_path('../../../', __FILE__)
-    File.expand_path(dummy_path, spec_folder_path)
-  end
+
+  let(:dummy_path) { 'spec/dummy' }
 
   before do
-    params = %W{. -q -f --skip-bundle -T -G}
-    params << '--dummy-path=%s' % dummy_path
-    Rails::Dummy::Generator.start(params)
+    @current_path = Dir.pwd
   end
 
   after do
+    FileUtils.cd @current_path
+    full_dummy_path = File.join(@current_path, dummy_path)
     FileUtils.rm_rf(full_dummy_path) if File.exist?(full_dummy_path)
   end
 
   it 'creates a dummy app' do
-    File.exist?(full_dummy_path).should be_true
+    params = %W{. -q -f --skip-bundle -T -G --dummy-path}
+    params << dummy_path
+    Rails::Dummy::Generator.start(params)
+
+    Dir[File.join(@current_path, dummy_path, '*')].should_not be_empty
   end
 end
