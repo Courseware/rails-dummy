b84d471a1ac37b4f5d1e4ce02e019605c1cee69c
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-07-09 14:39:18 -0400

    use alias_method instead of alias

--- lib/rails/dummy/generator.rb
+++ lib/rails/dummy/generator.rb
@@ -13,19 +13,18 @@ module Rails
       def do_nothing
       end
 
-      alias :create_root :do_nothing
-      alias :create_root_files :do_nothing
-      alias :create_app_files :do_nothing
-      alias :create_config_files :do_nothing
-      alias :create_lib_files :do_nothing
-      alias :create_public_stylesheets_files :do_nothing
-      alias :create_javascript_files :do_nothing
-      alias :create_script_files :do_nothing
-      alias :update_gemfile :do_nothing
-      alias :create_test_files :do_nothing
-      alias :finish_template :do_nothing
-      alias :create_bin_files :do_nothing
-
+      alias_method :create_root, :do_nothing
+      alias_method :create_root_files, :do_nothing
+      alias_method :create_app_files, :do_nothing
+      alias_method :create_config_files, :do_nothing
+      alias_method :create_lib_files, :do_nothing
+      alias_method :create_public_stylesheets_files, :do_nothing
+      alias_method :create_javascript_files, :do_nothing
+      alias_method :create_script_files, :do_nothing
+      alias_method :update_gemfile, :do_nothing
+      alias_method :create_test_files, :do_nothing
+      alias_method :finish_template, :do_nothing
+      alias_method :create_bin_files, :do_nothing
     end
   end
 end
